c39faaa8859c39ac62caa1081d878faa82766567
Jose Alonso <mail@josealonso.dev>
2023-09-09 10:35:07 -0600

    CleanCode: Application. Delete Command

---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveType/Commands/DeleteLeaveType/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using CleanCode.Core.Application.Contracts.Persistence;
+using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveType.Commands.DeleteLeaveType;
+
+public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand, Unit>
+{
+  
+    private readonly ILeaveTypeRepository _repository;
+
+    public DeleteLeaveTypeCommandHandler(
+        ILeaveTypeRepository leaveTypeRepository)
+    {
+        
+        _repository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveTypeCommand request
+        , CancellationToken cancellationToken)
+    {
+        // Get the object from database
+        var leaveTypeToDelete = await _repository.GetByIdAsync(request.Id);
+
+        // convert to domain entity object
+        
+
+        // delete from database
+        await _repository.DeleteAsync(leaveTypeToDelete);
+
+        // return Unit value
+        return Unit.Value;
+    }
+}
