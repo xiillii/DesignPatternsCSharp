1c8a8b58d7b657d53d14316677afaf55bd2786a8
Jose Alonso <mail@josealonso.dev>
2023-09-09 01:42:52 -0600

    CleanCode: Application. Features folder
    Vertical Slice Architecture

---
+++ 00CleanCode/src/core/CleanCode.Core.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CleanCode.Core.Application;
+
+public static class ApplicationServiceRegistration
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        services.AddAutoMapper(assembly);
+        services.AddMediatR(conf =>
+            conf.RegisterServicesFromAssemblies(assembly));
+
+        return services;
+    }
+}
---
+++ 00CleanCode/src/core/CleanCode.Core.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CleanCode.Core.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using CleanCode.Core.Domain;
+
+namespace CleanCode.Core.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+    }
+}
