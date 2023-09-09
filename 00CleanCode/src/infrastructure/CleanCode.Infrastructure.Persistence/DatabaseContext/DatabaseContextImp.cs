c54219c4a5d7b6bd81ed615d55de952d9562e10d
Jose Alonso <mail@josealonso.dev>
2023-09-09 13:18:28 -0600

    CleanCode: Persitence. Database context

---
+++ 00CleanCode/src/infrastructure/CleanCode.Infrastructure.Persistence/DatabaseContext/DatabaseContextImp.cs
@@ -0,0 +1,49 @@
+﻿using CleanCode.Core.Domain;
+using CleanCode.Core.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanCode.Infrastructure.Persistence.DatabaseContext;
+
+public class DatabaseContextImp : DbContext
+{
+    public DatabaseContextImp(DbContextOptions<DatabaseContextImp> options) : base(options)
+    {
+    }
+
+    public DbSet<LeaveType> LeaveTypes { get; set; }
+    public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+    public DbSet<LeaveRequest> LeaveRequests { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DatabaseContextImp).Assembly);
+
+        modelBuilder.Entity<LeaveType>().HasData(
+            new LeaveType
+            {
+                Id = 1,
+                Name = "Vacation",
+                DefaultDays = 10,
+                DateCreated = DateTime.Now,
+                DateModified = DateTime.Now,
+            });
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        foreach (var entry in base.ChangeTracker.Entries<BaseEntity>()
+            .Where(q => q.State == EntityState.Added || q.State == EntityState.Modified))
+        {
+            entry.Entity.DateModified = DateTime.UtcNow;
+
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.DateCreated = DateTime.UtcNow;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
---
+++ 00CleanCode/src/infrastructure/CleanCode.Infrastructure.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanCode.Infrastructure.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
