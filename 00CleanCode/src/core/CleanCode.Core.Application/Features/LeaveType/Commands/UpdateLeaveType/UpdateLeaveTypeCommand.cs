79abacef68aaece7ac350ae50decfbae8089877a
Jose Alonso <mail@josealonso.dev>
2023-09-09 10:27:35 -0600

    CleanCode: Application. Update Command

---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class UpdateLeaveTypeCommand : IRequest<int>
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveType/Commands/UpdateLeaveType/UpdateLeaveTypeCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using CleanCode.Core.Application.Contracts.Persistence;
+using CleanCode.Core.Domain;
+using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveType.Commands.CreateLeaveType;
+
+public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand, Unit>
+{
+    private readonly IMapper _mapper;
+    private readonly ILeaveTypeRepository _repository;
+
+    public UpdateLeaveTypeCommandHandler(IMapper mapper,
+        ILeaveTypeRepository leaveTypeRepository)
+    {
+        _mapper = mapper;
+        _repository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(UpdateLeaveTypeCommand request
+        , CancellationToken cancellationToken)
+    {
+        // validate incoming data
+
+        // convert to domain entity object
+        var leaveTypeToUpdate = _mapper.Map<Domain.LeaveType>(request);
+
+        // add to database
+        await _repository.CreateAsync(leaveTypeToUpdate);
+
+        // return Unit value
+        return Unit.Value;
+    }
+}
