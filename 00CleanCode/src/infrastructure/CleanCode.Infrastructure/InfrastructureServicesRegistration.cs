4c5afb3a5413a9bb3c7782e120f5f22e3255d856
Jose Alonso <mail@josealonso.dev>
2023-09-09 17:55:31 -0600

    CleanCode: Infrastructure. Register Aws EmailSender

--- 00CleanCode/src/infrastructure/CleanCode.Infrastructure/InfrastructureServicesRegistration.cs
+++ 00CleanCode/src/infrastructure/CleanCode.Infrastructure/InfrastructureServicesRegistration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using CleanCode.Core.Application.Contracts.Email;
+using CleanCode.Core.Application.Models.Email;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanCode.Infrastructure;
@@ -8,6 +10,8 @@ public static class InfrastructureServicesRegistration
     public static IServiceCollection ConfigureInfrastructureServices(
         this IServiceCollection services, IConfiguration configuration)
     {
+        services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+        services.AddTransient<IEmailSender, EmailService.Aws.EmailSender>();
 
         return services;
     }
