5cfdc505384d921a4a4b1a1cbc5a5cbbba29d070
Jose Alonso <mail@josealonso.dev>
2023-09-09 13:39:15 -0600

    CodeClean: Persistence. Initial repository implementation

---
+++ 00CleanCode/src/infrastructure/CleanCode.Infrastructure.Persistence/Repositories/GenericRepositoryImpl.cs
@@ -0,0 +1,31 @@
+﻿using CleanCode.Core.Application.Contracts.Persistence;
+
+namespace CleanCode.Infrastructure.Persistence.Repositories;
+
+public class GenericRepositoryImpl<T> : IGenericRepository<T> where T : class
+{
+    public Task<T> CreateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<T>> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
