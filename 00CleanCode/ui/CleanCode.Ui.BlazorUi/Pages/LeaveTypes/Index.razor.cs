6f1066c671fa67afac8812a2ea34fab240bf1b45
Jose Alonso <mail@josealonso.dev>
2023-09-10 16:38:35 -0600

    CleanCode: UI. Blazor UI. LeaveType Page UI

---
+++ 00CleanCode/ui/CleanCode.Ui.BlazorUi/Pages/LeaveTypes/Index.razor.cs
@@ -0,0 +1,56 @@
+using CleanCode.Ui.BlazorUi.Contracts;
+using CleanCode.Ui.BlazorUi.Models.LeaveTypes;
+using Microsoft.AspNetCore.Components;
+
+namespace CleanCode.Ui.BlazorUi.Pages.LeaveTypes;
+
+public partial class Index
+{
+    [Inject]
+    public NavigationManager NavigationManager { get; set; }
+
+    [Inject]
+    public ILeaveTypeService LeaveTypeService { get; set; }
+
+    public List<LeaveTypeVM> LeaveTypes { get; set; }
+    public string Message { get; set; } = string.Empty;
+
+    protected void CreateLeaveType()
+    {
+        NavigationManager.NavigateTo("/leavetypes/create/");
+    }
+
+    protected void AllocateLeaveType(int id)
+    {
+        // TODO: use leave allocation service here
+    }
+
+    protected void EditLeaveType(int id)
+    {
+        NavigationManager.NavigateTo($"/leavetypes/edit/{id}");
+    }
+
+    protected void DetailsLeaveType(int id)
+    {
+        NavigationManager.NavigateTo($"/leavetypes/details/{id}");
+    }
+
+    protected async Task DeleteLeaveType(int id)
+    {
+        var response = await LeaveTypeService.DeleteLeaveType(id);
+
+        if (response.Success)
+        {
+            await OnInitializedAsync();
+        }
+        else
+        {
+            Message = response.Message;
+        }
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        LeaveTypes = await LeaveTypeService.GetLeaveTypes();
+    }
+}
