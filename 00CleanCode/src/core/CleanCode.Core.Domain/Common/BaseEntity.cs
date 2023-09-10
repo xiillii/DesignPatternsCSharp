03ea2f85d949c04f59b9ae252fa82ed467b23d2a
Jose Alonso <mail@josealonso.dev>
2023-09-10 11:56:11 -0600

    CleanCode. Domain. Fix Datetimes
    To be nulleable

--- 00CleanCode/src/core/CleanCode.Core.Domain/Common/BaseEntity.cs
+++ 00CleanCode/src/core/CleanCode.Core.Domain/Common/BaseEntity.cs
@@ -3,6 +3,6 @@
 public abstract class BaseEntity
 {
     public int Id { get; set; }
-    public DateTime DateCreated { get; set; }
-    public DateTime DateModified { get; set; }
+    public DateTime? DateCreated { get; set; }
+    public DateTime? DateModified { get; set; }
 }
