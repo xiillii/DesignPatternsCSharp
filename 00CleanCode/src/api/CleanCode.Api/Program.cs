7c01f37648fec4e6615d2347108fb6e636d1f1ab
Jose Alonso <mail@josealonso.dev>
2023-09-09 19:26:43 -0600

    CleanCode: Api. Configurate Project

--- 00CleanCode/src/api/CleanCode.Api/Program.cs
+++ 00CleanCode/src/api/CleanCode.Api/Program.cs
@@ -1,8 +1,23 @@
+using CleanCode.Core.Application;
+using CleanCode.Infrastructure;
+using CleanCode.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddPersistenceServices(builder.Configuration);
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(opts =>
+{
+    opts.AddPolicy("all", builder => builder.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
