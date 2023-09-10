4c9fc8138e826f55eb407aef732f2826ee27e99e
Jose Alonso <mail@josealonso.dev>
2023-09-09 23:04:34 -0600

    CleanCode: Application. Commands. Delete LeaveAllocation

---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveAllocation/Commads/DeleteLeaveAllocation/DeleteLeaveAllocationCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveAllocation.Commads.DeleteLeaveAllocation;
+
+public class DeleteLeaveAllocationCommand : IRequest<Unit>
+{
+    public int Id { get; set; }
+}
---
+++ 00CleanCode/src/core/CleanCode.Core.Application/Features/LeaveAllocation/Commads/DeleteLeaveAllocation/DeleteLeaveAllocationCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CleanCode.Core.Application.Contracts.Persistence;
+using CleanCode.Core.Application.Exceptions;
+using MediatR;
+
+namespace CleanCode.Core.Application.Features.LeaveAllocation.Commads.DeleteLeaveAllocation;
+
+public class DeleteLeaveAllocationCommandHandler
+    : IRequestHandler<DeleteLeaveAllocationCommand, Unit>
+{
+    private readonly ILeaveAllocationRepository _repository;
+
+    public DeleteLeaveAllocationCommandHandler(
+        ILeaveAllocationRepository leaveTypeRepository)
+    {
+
+        _repository = leaveTypeRepository;
+    }
+
+    public async Task<Unit> Handle(DeleteLeaveAllocationCommand request, CancellationToken cancellationToken)
+    {
+        // get the object from database
+        var lAllocationToDelete = await _repository
+            .GetByIdAsync(request.Id) ??
+                throw new NotFoundException(nameof(Domain.LeaveAllocation), request.Id);
+
+        // delete from database
+        await _repository.UpdateAsync(lAllocationToDelete);
+
+        // return
+        return Unit.Value;
+    }
+}
